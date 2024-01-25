2f2d5e9483d3717f565c3a1130e0893c2b676708
Bhoowan <bhoowankapoor.bk@gmail.com>
2024-01-25 09:56:41 +0200

    random_randint

---
+++ random_randint.py
@@ -0,0 +1,7 @@
+import random
+
+random_number1 = random.randint(0, 9)
+random_number2 = random.randint(0, 9)
+random_number3 = random.randint(0, 9)
+
+print(f'The code for your suitcase is: {random_number1}{random_number2}{random_number3}')
