d8b0a0b1a9e89f10dc9f7e7f78232790095a6021
Bhoowan <bhoowankapoor.bk@gmail.com>
2024-01-25 11:40:16 +0200

    if-else

--- if_else.py
+++ if_else.py
@@ -1,6 +1,3 @@
 user = input('Enter your Name: ')
-print("Hello, " + user + '!')
-age = int(input('Enter your Age: '))
-if age >= 18:
-    print("You are eligible to take the license")
-
+user1 = user.lower()
+print(user1)
